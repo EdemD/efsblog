b32080c1cb7e225c7325c76d8b3038aa19de83e4
Edem Dosseh <edemdosseh@gmail.com>
2019-03-11 15:38:41 -0500

    Update clock.py

--- efsblog/clock.py
+++ efsblog/clock.py
@@ -3,46 +3,48 @@ import psycopg2
 
 sched = BlockingScheduler()
 
-
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
 
-# import psycopg2
+	print('This job is run every three minutes.')
+	
+	try:
+		connection = psycopg2.connect(user="lcpswybvhdtxgt",
+									  password="eb128c6e75b41212a5709223ffa9ec58f975939f2267bf262e07e93729e222f3",
+									  host="ec2-54-83-203-198.compute-1.amazonaws.com",
+									  port="5432",
+									  database="d5k81ffjrgb3gr",
+									  sslmode="require")
+		cursor = connection.cursor()
+		# Print PostgreSQL Connection properties
+		print(connection.get_dsn_parameters(), "\n")
+		# Print PostgreSQL version
+		cursor.execute("SELECT version();")
+		record = cursor.fetchone()
+		print("You are connected to - ", record, "\n")
+		
+		cursor.execute("SELECT * from Customer;")
+		record = cursor.fetchone()
+		print("Here are our customers - ", record, "\n")
+		
+		cursor.close()
+		connection.close()
+		# print("PostgreSQL connection is closed")
+	except (Exception, psycopg2.Error) as error:
+		print("Error while connecting to PostgreSQL", error)
+	finally:
+		# closing database connection.
+		# if (connection):
+		# cursor.close()
+		# connection.close()
+		print("PostgreSQL connection is closed")
 
+		# store all the fetched data in the ans variable
 
-try:
-    connection = psycopg2.connect(user="lcpswybvhdtxgt",
-                                  password="eb128c6e75b41212a5709223ffa9ec58f975939f2267bf262e07e93729e222f3",
-                                  host="ec2-54-83-203-198.compute-1.amazonaws.com",
-                                  port="5432",
-                                  database="d5k81ffjrgb3gr",
-                                  sslmode="require")
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print(connection.get_dsn_parameters(), "\n")
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
-    cursor.close()
-    connection.close()
-    # print("PostgreSQL connection is closed")
-except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-finally:
-    # closing database connection.
-    # if (connection):
-    # cursor.close()
-    # connection.close()
-    print("PostgreSQL connection is closed")
+		# ans= cursor.fetchall()
 
-    # store all the fetched data in the ans variable
-
-    # ans= crsr.fetchall()
-
-    # loop to print all the data
-    # for i in ans:
-    #    print(i)
+		# loop to print all the data
+		# for i in ans:
+		#    print(i)
 
 sched.start()
